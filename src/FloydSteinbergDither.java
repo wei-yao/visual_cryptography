d56b2eb72d088c471e3698ba2c414200f09ac30d
weiyao <1033789547@qq.com>
2014-11-12 01:16:49 +0800

    test fail

--- src/FloydSteinbergDither.java
+++ src/FloydSteinbergDither.java
@@ -14,198 +14,98 @@ Retrieved from: http://en.literateprograms.org/Floyd-Steinberg_dithering_(Java)?
  */
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-//class RGBTriple {
-//    public final byte[] channels;
-//    public RGBTriple() { channels = new byte[3]; }
-//    public RGBTriple(int R, int G, int B)
-//    { channels = new byte[]{(byte)R, (byte)G, (byte)B}; }
-//}
+class RGBTriple {
+    public final byte[] channels;
+
+    public RGBTriple() {
+        channels = new byte[3];
+    }
+
+    public RGBTriple(final int R, final int G, final int B)
+    {
+        channels = new byte[] {
+                (byte) R, (byte) G, (byte) B
+        };
+    }
+}
 
 public class FloydSteinbergDither
 {
-    /**
-     * 标志输出格式（位深度).
-     */
-    private final int outputFormat;
-    /**
-     * 色值的数组，定义输出图像含有的色值类型.
-     */
-    private final Color[] palette;
-
-    public FloydSteinbergDither(final Color[] color, final int outputFormat) {
-        this.palette = new Color[color.length];
-        System.arraycopy(color, 0, this.palette, 0, color.length);
-        this.outputFormat = outputFormat;
-    }
-
-    /**
-     * 加色值
-     * 
-     * @param a
-     * @param b
-     * @return
-     */
-    private static byte plus_truncate_uchar(final int a, final int b) {
+    private static byte plus_truncate_uchar(final byte a, final int b) {
         if ((a & 0xff) + b < 0) {
             return 0;
         } else if ((a & 0xff) + b > 255) {
             return (byte) 255;
         } else {
-            return (byte) (a & 0xff + b);
+            return (byte) (a + b);
         }
     }
 
-    private Color findNearestColor(final Color color) {
+    private static byte findNearestColor(final RGBTriple color, final RGBTriple[] palette) {
         int minDistanceSquared = 255 * 255 + 255 * 255 + 255 * 255 + 1;
-        byte bestIndex = 1;
+        byte bestIndex = 0;
         for (byte i = 0; i < palette.length; i++) {
-
-            int Rdiff = (color.getRed() & 0xff) - (palette[i].getRed() &
-                    0xff);
-            int Gdiff = (color.getGreen() & 0xff) - (palette[i].getGreen() &
-                    0xff);
-            int Bdiff = (color.getBlue() & 0xff) - (palette[i].getBlue() &
-                    0xff);
-            int distanceSquared = Rdiff * Rdiff + Gdiff * Gdiff + Bdiff *
-                    Bdiff;
+            int Rdiff = (color.channels[0] & 0xff) - (palette[i].channels[0] & 0xff);
+            int Gdiff = (color.channels[1] & 0xff) - (palette[i].channels[1] & 0xff);
+            int Bdiff = (color.channels[2] & 0xff) - (palette[i].channels[2] & 0xff);
+            int distanceSquared = Rdiff * Rdiff + Gdiff * Gdiff + Bdiff * Bdiff;
             if (distanceSquared < minDistanceSquared) {
                 minDistanceSquared = distanceSquared;
                 bestIndex = i;
             }
         }
-        // if (color.getRed() < 125 || color.getGreen() < 125 || color.getBlue()
-        // < 125)
-        // {
-        // bestIndex = 0;
-        // } else {
-        // int a = 0;
-        // }
-        return palette[bestIndex];
+        return bestIndex;
     }
 
-    private int getWeight(final int i, final int j) {
-        int ret = 0;
-        if (i == 1 && j == 0) {
-            ret = 7;
-        } else if (i == -1 && j == 1) {
-            ret = 3;
-        } else if (i == 0 && j == 1) {
-            ret = 5;
-        } else if (i == 1 && j == 1) {
-            ret = 1;
-        }
-        return ret;
-    }
-
-    private BufferedImage floydSteinbergDither(final BufferedImage image)
+    public static byte[][] floydSteinbergDither(final RGBTriple[][] image, final RGBTriple[] palette)
     {
-        // byte[][] result = new byte[image.length][image[0].length];
-        BufferedImage imgCopy = new BufferedImage(image.getWidth(), image.getHeight(),
-                BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = imgCopy.createGraphics();
-        g.drawImage(image, 0, 0, null);
-        int count = 0;
-        int countW = 0;
-        for (int y = 0; y < image.getHeight(); y++) {
-            for (int x = 0; x < image.getWidth(); x++) {
+        byte[][] result = new byte[image.length][image[0].length];
 
-                Color currentPixel = new Color(imgCopy.getRGB(x, y));
-                Color dstPixel = findNearestColor(currentPixel);
-                if (dstPixel == Color.BLACK) {
-                    count++;
-                } else {
-                    countW++;
-                }
-                imgCopy.setRGB(x, y, dstPixel.getRGB());
-                // g.setColor(dstPixel);
-                // g.fillRect(x, y, 1, 1);
-                for (int i = -1; i < 2 && x + i < image.getWidth() && x + i
-                        >= 0; i++) {
-                    for (int j = 0; j < 2 && y + j < image.getHeight(); j++) {
-                        if (i == -1 && j == 0) {
-                            continue;
-                        }
-                        if (i == 0 && j == 0) {
-                            continue;
-                        }
-                        byte channels[] = new byte[3];
-                        final int weight = getWeight(i, j);
-                        Color targetPixel = new Color(imgCopy.getRGB(x + i, y + j));
-                        for (int k = 0; k < 3; k++) {
-                            int error = (getChannel(currentPixel, k))
-                                    - getChannel(dstPixel, k);
-                            channels[k] =
-                                    plus_truncate_uchar(getChannel(targetPixel, k),
-                                            (error * weight) >> 4);
-                        }
-                        try {
-                            Color pixelColor = new Color(channels[0] & 0xff, channels[1]
-                                    & 0xff,
-                                    channels[2] & 0xff,
-                                    targetPixel.getAlpha());
-                            // g.setColor(pixelColor);
-                            // g.drawRect(x + i, y + j, 1, 1);
-                            imgCopy.setRGB(x + i, y + j,
-                                    pixelColor.getRGB());
-                        } catch (IllegalArgumentException e) {
-                            System.out.println((channels[0] & 0xff) + " " + channels[1] +
-                                    " "
-                                    + channels[2]);
-                            throw e;
-                        }
+        for (int y = 0; y < image.length; y++) {
+            for (int x = 0; x < image[y].length; x++) {
+                RGBTriple currentPixel = image[y][x];
+                byte index = findNearestColor(currentPixel, palette);
+                result[y][x] = index;
 
+                for (int i = 0; i < 3; i++)
+                {
+                    int error = (currentPixel.channels[i] & 0xff)
+                            - (palette[index].channels[i] & 0xff);
+                    if (x + 1 < image[0].length) {
+                        image[y + 0][x + 1].channels[i] =
+                                plus_truncate_uchar(image[y + 0][x + 1].channels[i],
+                                        (error * 7) >> 4);
+                    }
+                    if (y + 1 < image.length) {
+                        if (x - 1 > 0) {
+                            image[y + 1][x - 1].channels[i] =
+                                    plus_truncate_uchar(image[y + 1][x - 1].channels[i],
+                                            (error * 3) >> 4);
+                        }
+                        image[y + 1][x + 0].channels[i] =
+                                plus_truncate_uchar(image[y + 1][x + 0].channels[i],
+                                        (error * 5) >> 4);
+                        if (x + 1 < image[0].length) {
+                            image[y + 1][x + 1].channels[i] =
+                                    plus_truncate_uchar(image[y + 1][x + 1].channels[i],
+                                            (error * 1) >> 4);
+                        }
                     }
                 }
             }
         }
-        System.out.println("count " + count + "　" + countW);
-        g.dispose();
-        return imgCopy;
-    }
-
-    public BufferedImage transform(final BufferedImage image) {
-        BufferedImage imgTemp = floydSteinbergDither(image);
-        if (imgTemp.getType() != outputFormat) {
-            BufferedImage imgCopy = new BufferedImage(image.getWidth(), image.getHeight(),
-                    outputFormat);
-            Graphics2D g = imgCopy.createGraphics();
-            g.drawImage(imgTemp, 0, 0, null);
-            g.dispose();
-            return imgCopy;
-        } else {
-            return imgTemp;
-        }
-
-    }
-
-    private int getChannel(final Color color, final int index) {
-        int ret = 0;
-        switch (index) {
-            case 0:
-                ret = color.getRed();
-                break;
-            case 1:
-                ret = color.getGreen();
-                break;
-            case 2:
-                ret = color.getBlue();
-                break;
-            default:
-                break;
-        }
-        return ret;
+        return result;
     }
 
     public static void main(final String[] args) throws IOException {
         // RGBTriple[][] image = new RGBTriple[145][100];
-
+        //
         // InputStream raw_in = new BufferedInputStream(new
         // FileInputStream(args[0]));
         // for (int y = 0; y < image.length; y++) {
@@ -214,45 +114,58 @@ public class FloydSteinbergDither
         // raw_in.read(image[y][x].channels, 0, 3);
         // }
         // }
+        BufferedImage inputImg = ImageIO.read(new File("input.bmp"));
+        RGBTriple[][] image = new RGBTriple[inputImg.getHeight()][inputImg.getWidth()];
+        for (int y = 0; y < image.length; y++) {
+            for (int x = 0; x < image[0].length; x++) {
+                Color color = new Color(inputImg.getRGB(x, y));
+                image[y][x] = new RGBTriple(color.getRed(), color.getGreen(), color.getBlue());
+                // raw_in.read(image[y][x].channels, 0, 3);
+            }
+        }
+
         // raw_in.close();
 
-        Color[] colors = {
-                new Color(149, 91, 110),
-                new Color(176, 116, 137),
-                new Color(17, 11, 15),
-                new Color(63, 47, 69),
-                new Color(93, 75, 112),
-                new Color(47, 62, 24),
-                new Color(76, 90, 55),
-                new Color(190, 212, 115),
-                new Color(160, 176, 87),
-                new Color(116, 120, 87),
-                new Color(245, 246, 225),
-                new Color(148, 146, 130),
-                new Color(200, 195, 180),
-                new Color(36, 32, 27),
-                new Color(87, 54, 45),
-                new Color(121, 72, 72)
+        RGBTriple[] palette = {
+                new RGBTriple(149, 91, 110),
+                new RGBTriple(176, 116, 137),
+                new RGBTriple(17, 11, 15),
+                new RGBTriple(63, 47, 69),
+                new RGBTriple(93, 75, 112),
+                new RGBTriple(47, 62, 24),
+                new RGBTriple(76, 90, 55),
+                new RGBTriple(190, 212, 115),
+                new RGBTriple(160, 176, 87),
+                new RGBTriple(116, 120, 87),
+                new RGBTriple(245, 246, 225),
+                new RGBTriple(148, 146, 130),
+                new RGBTriple(200, 195, 180),
+                new RGBTriple(36, 32, 27),
+                new RGBTriple(87, 54, 45),
+                new RGBTriple(121, 72, 72)
         };
-        // Color colors[] = {
-        // Color.BLACK,
-        // Color.WHITE
-        // };
-        FloydSteinbergDither fsd = new FloydSteinbergDither(colors, BufferedImage.TYPE_INT_ARGB);
-        // byte[][] result = floydSteinbergDither(image, palette);
-        File sourceFile = new File("input.bmp");
-        BufferedImage imgSrc = ImageIO.read(sourceFile);
-        BufferedImage output = fsd.transform(imgSrc);
-        saveImage(output);
-
-    }
-
-    /**
-     * @param output
-     * @throws IOException
-     */
-    public static void saveImage(final BufferedImage output) throws IOException {
-        File outFile = new File("output.png");
-        ImageIO.write(output, "png", outFile);
+        BufferedImage output = new BufferedImage(inputImg.getWidth(), inputImg.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        byte[][] result = floydSteinbergDither(image, palette);
+        for (int y = 0; y < result.length; y++) {
+            for (int x = 0; x < result[0].length; x++) {
+                RGBTriple rt = palette[result[y][x]];
+                Color color = new Color(rt.channels[0] & 0xff, rt.channels[1] & 0xff,
+                        rt.channels[2] & 0xff, 255);
+                output.setRGB(x, y, color.getRed());
+                // image[y][x] = new RGBTriple(color.getRed(), color.getGreen(),
+                // color.getBlue());
+                // raw_in.read(image[y][x].channels, 0, 3);
+            }
+        }
+        // OutputStream raw_out = new BufferedOutputStream(new
+        // FileOutputStream(args[1]));
+        // for (int y = 0; y < result.length; y++) {
+        // for (int x = 0; x < result[y].length; x++) {
+        // raw_out.write(palette[result[y][x]].channels, 0, 3);
+        // }
+        // }
+        ImageIO.write(output, "png", new File("output.png"));
+        // raw_out.close();
     }
 }
